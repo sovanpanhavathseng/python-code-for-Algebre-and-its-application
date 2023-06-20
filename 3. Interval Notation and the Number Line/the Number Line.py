72c6b7fdbc4fd063f48c51a0c7ebae4d550ef294
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-06-20 12:05:45 +0700

    Create the Number Line.py

---
+++ 3. Interval Notation and the Number Line/the Number Line.py
@@ -0,0 +1,15 @@
+def number_line(start, end):
+  """Prints the numbers on the number line from start to end, inclusive."""
+  for i in range(start, end + 1):
+    print(i)
+
+if __name__ == "__main__":
+  number_line(1, 10)
+
+// This code first defines a function called number_line(). This function takes two arguments: start and end. The start argument specifies the first number on the number line, and the end argument specifies the last number on the number line.
+
+// The number_line() function then uses a for loop to iterate through all the numbers from start to end, inclusive. In each iteration of the loop, the print() function is used to print the current number.
+
+// The number_line() function is then called with the arguments 1 and 10. This tells the function to print the numbers from 1 to 10, inclusive.
+
+// To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as number_line.py, you can run it by typing the following command into the command line:
