efb3045d0044b94ced8f5fbf44aa171127b2727e
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-06-24 11:15:19 +0700

    Create The Cartesian Coordinate System.py

---
+++ 10. The Cartesian Coordinate System/The Cartesian Coordinate System.py
@@ -0,0 +1,25 @@
+import math
+
+def CartesianCoordinateSystem(x, y):
+  """
+  This function takes two numbers, x and y, and prints them as a Cartesian coordinate.
+
+  Args:
+    x: The x-coordinate.
+    y: The y-coordinate.
+
+  Returns:
+    None.
+  """
+
+  print("The Cartesian coordinate is ({}, {}).".format(x, y))
+
+if __name__ == "__main__":
+  x = 3
+  y = 4
+
+  CartesianCoordinateSystem(x, y)
+
+// This code first defines a function called CartesianCoordinateSystem(). This function takes two numbers, x and y, as input and prints them as a Cartesian coordinate. The function then defines a variable called x and assigns it the value 3. The function then defines a variable called y and assigns it the value 4. The function then calls the CartesianCoordinateSystem() function, passing in the values of x and y as arguments. The CartesianCoordinateSystem() function then prints the Cartesian coordinate, which is (3, 4).
+
+// To run this code, you can save it as a Python file and then run it from the command line. For example, if you save the code as cartesian_coordinate_system.py, you can run it by typing the following command into the command line:
