ecbf80677e891a1f9a392357a69046904deb1be0
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-06-30 11:49:58 +0700

    Create Real-Valued Functions of a Real Variable.py

---
+++ 16. Real-Valued Functions of a Real Variable/Real-Valued Functions of a Real Variable.py
@@ -0,0 +1,16 @@
+def f(x):
+  """Returns the square of x."""
+  return x ** 2
+
+def main():
+  """Prints the value of f(2)."""
+  print(f(2))
+
+if __name__ == "__main__":
+  main()
+
+// This code defines a function called f that takes a real number as input and returns the square of that number. The function is defined using the def keyword. The body of the function is indented, and it contains a single line of code that returns the square of the input number.
+
+// The main function calls the f function with the input number 2. The f function returns the value 4, which is then printed to the console.
+
+// To run this code, you can save it as a Python file and then run the file from the command line. For example, if you save the code as real_valued_functions.py, you can run it by typing the following command into the command line:
