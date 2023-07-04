3930c582f8b7157b7ffa113bc82fe8333aab88ea
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-07-04 21:59:23 +0700

    Create Multivariable Functions.py

---
+++ 18. Multivariable Functions/Multivariable Functions.py
@@ -0,0 +1,13 @@
+def f(x, y):
+  """Returns the square of the sum of x and y."""
+  return x**2 + y**2
+
+
+if __name__ == "__main__":
+  print(f(1, 2))  # Prints 5
+  print(f(2, 3))  # Prints 13
+  print(f(3, 4))  # Prints 25
+
+// This code defines a function called f that takes two numbers as input (x and y) and returns a single number as output (x^2 + y^2). The if __name__ == "__main__": block is used to run the code if the file is being run as a script, rather than imported as a module.
+
+// To run this code, you can save it as a Python file and then run the file from the command line. For example, if you save the code as multivariable_function.py, you can run it by typing the following command into the command line:
