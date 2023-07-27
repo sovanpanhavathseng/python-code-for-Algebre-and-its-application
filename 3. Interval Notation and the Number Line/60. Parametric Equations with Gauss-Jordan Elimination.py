fcecc7d08a6b21d962c01db9a9ac5b5510041ccc
Jonathanseng <corodostudio@gmail.com>
2023-07-27 20:30:58 +0700

    Parametric Equations with Gauss-Jordan Elimination

---
+++ 3. Interval Notation and the Number Line/60. Parametric Equations with Gauss-Jordan Elimination.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+def gauss_jordan_elimination(A, b):
+  """Performs Gauss-Jordan elimination on the matrix A and vector b.
+
+  Args:
+    A: The coefficient matrix.
+    b: The right-hand side vector.
+
+  Returns:
+    The solution vector x.
+  """
+
+  n = A.shape[0]
+  for i in range(n):
+    # Find the row with the largest leading entry in the current column.
+    max_row = i
+    for j in range(i + 1, n):
+      if abs(A[j, i]) > abs(A[max_row, i]):
+        max_row = j
+
+    # Swap rows i and max_row.
+    A[[i, max_row]] = A[[max_row, i]]
+    b[[i, max_row]] = b[[max_row, i]]
+
+    # Scale the row so that the leading entry is 1.
+    if A[i, i] != 0:
+      A[i, :] = A[i, :] / A[i, i]
+    b[i] = b[i] / A[i, i]
+
+    # Subtract multiples of the leading row from the other rows.
+    for j in range(i + 1, n):
+      if A[j, i] != 0:
+        factor = A[j, i] / A[i, i]
+        A[j, :] -= factor * A[i, :]
+        b[j] -= factor * b[i]
+
+  return b
+
+def main():
+  """Runs the Gauss-Jordan elimination algorithm."""
+
+  A = np.array([[1, 2, -3], [-2, 1, 4], [3, -1, -2]])
+  b = np.array([1, 2, 3])
+
+  x = gauss_jordan_elimination(A, b)
+  print(x)
+
+  t = np.linspace(0, 1, 100)
+  x1 = x[0] * t + x[1]
+  x2 = x[2] * t + x[3]
+
+  plt.plot(t, x1, label='x1')
+  plt.plot(t, x2, label='x2')
+  plt.legend()
+  plt.show()
+
+if __name__ == "__main__":
+  main()
