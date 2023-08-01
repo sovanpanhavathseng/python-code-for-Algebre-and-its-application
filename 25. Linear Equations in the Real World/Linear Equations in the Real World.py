30a5ef9df562e94d58d3f383e51c16a73e373bf9
Jonathanseng <corodostudio@gmail.com>
2023-08-01 21:40:00 +0700

    Linear Equations in the Peal Would

--- 25. Linear Equations in the Real World/Linear Equations in the Real World.py
+++ 25. Linear Equations in the Real World/Linear Equations in the Real World.py
@@ -1,33 +1,33 @@
-def mileage_rate(distance, hours):
-  """Calculates the mileage rate given the distance and hours."""
-  mileage_rate = distance / hours
-  return mileage_rate
+import numpy as np
 
-def income_estimate(current_income, annual_increase, years):
-  """Estimates the income in the future given the current income, annual increase, and years."""
-  estimated_income = current_income + annual_increase * years
-  return estimated_income
+def least_squares_approximation(x, y):
+  """
+  Performs least squares approximation on the data points (x, y).
 
-def compare_rates(job_1, job_2):
-  """Compares the rates of pay for two jobs."""
-  if job_1["salary"] > job_2["salary"]:
-    print("Job 1 pays a higher rate.")
-  elif job_1["salary"] < job_2["salary"]:
-    print("Job 2 pays a higher rate.")
-  else:
-    print("The two jobs pay the same rate.")
+  Args:
+    x: The x-coordinates of the data points.
+    y: The y-coordinates of the data points.
 
-def profit_prediction(cost, price):
-  """Predicts the profit given the cost and price."""
-  profit = price - cost
-  return profit
+  Returns:
+    m: The slope of the least squares line.
+    b: The y-intercept of the least squares line.
+  """
 
-if __name__ == "__main__":
-  print("Mileage rate:", mileage_rate(100, 2))
-  print("Income estimate:", income_estimate(50000, 5000, 5))
-  compare_rates({"salary": 40000}, {"salary": 20 * 60})
-  print("Profit prediction:", profit_prediction(10, 20))
+  n = len(x)
+  A = np.zeros((n, 2))
+  b = np.zeros(n)
 
-// This code shows how linear equations can be used to solve real-world problems. For example, the mileage_rate() function calculates the mileage rate given the distance and hours. The income_estimate() function estimates the income in the future given the current income, annual increase, and years. The compare_rates() function compares the rates of pay for two jobs. And the profit_prediction() function predicts the profit given the cost and price.
+  for i in range(n):
+    A[i, 0] = x[i]
+    A[i, 1] = 1
+    b[i] = y[i]
 
-// This is just a simple example of how linear equations can be used in Python. There are many other ways to use linear equations in Python, and the possibilities are endless.
+  m, b = np.linalg.lstsq(A, b)[0]
+
+  return m, b
+
+if name == "__main__":
+  x = np.array([1, 2, 3, 4, 5])
+  y = np.array([2, 4, 6, 8, 10])
+  m, b = least_squares_approximation(x, y)
+  print(m, b)
