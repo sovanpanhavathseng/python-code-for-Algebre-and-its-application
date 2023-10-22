e1edeb8ffba21a4f7058cafa3cf8b274bb419bb4
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2023-10-22 10:44:52 +0700

    Create 77. The Quadratic Formula.py

---
+++ Applications/77. The Quadratic Formula.py
@@ -0,0 +1,36 @@
+The quadratic formula is a mathematical formula that can be used to find the solutions (or roots) of a quadratic equation. A quadratic equation is an equation of the form ax² + bx + c = 0, where a, b, and c are constants and a ≠ 0.
+
+The quadratic formula is:
+
+```
+x = (-b ± √(b² - 4ac)) / 2a
+```
+
+where:
+
+* x is the solution of the quadratic equation
+* a, b, and c are the coefficients of the quadratic equation
+
+The term under the radical is called the discriminant. The discriminant determines the number and type of solutions that the quadratic equation has.
+
+* If the discriminant is positive (b² - 4ac > 0), then the quadratic equation has two real solutions.
+* If the discriminant is zero (b² - 4ac = 0), then the quadratic equation has one rational solution.
+* If the discriminant is negative (b² - 4ac < 0), then the quadratic equation has two complex solutions.
+
+The quadratic formula can be used to solve a wide variety of problems. Some common applications of the quadratic formula include:
+
+* Finding the roots of a quadratic equation
+* Finding the vertex of a parabola
+* Finding the maximum or minimum value of a quadratic function
+* Solving problems involving projectile motion
+* Solving problems involving areas and volumes
+
+The quadratic formula is a powerful tool that can be used to solve a wide variety of problems. By understanding the quadratic formula, you can gain a deeper understanding of algebra and its applications.
+
+Here are some examples of how the quadratic formula can be used to solve real-world problems:
+
+* **Finding the height of a projectile:** The height of a projectile can be modeled by the quadratic equation h = -16t² + vt + h₀, where h is the height, t is the time, v is the initial velocity, and h₀ is the initial height. The quadratic formula can be used to find the time at which the projectile reaches its maximum height.
+* **Finding the range of a projectile:** The range of a projectile can be modeled by the quadratic equation R = v²sin(2θ) / g, where R is the range, v is the initial velocity, θ is the launch angle, and g is the acceleration due to gravity. The quadratic formula can be used to find the launch angle that will result in the maximum range.
+* **Finding the dimensions of a rectangle:** The area of a rectangle can be modeled by the quadratic equation A = lw, where A is the area, l is the length, and w is the width. The quadratic formula can be used to find the length or width of a rectangle if the area and the other dimension are known.
+
+The quadratic formula is a versatile tool that can be used to solve a wide variety of problems. By understanding the quadratic formula, you can gain a deeper understanding of algebra and its applications.
